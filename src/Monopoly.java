31e2de8c6e03fcc4664967a0afe1bae385bf85ac
Elliot Roller <eroller9545@stu.neisd.net>
2025-01-31 13:09:06 -0600

    Save 3
    Co-authored-by: BrettMacKay <BrettMacKay@users.noreply.github.com>

--- src/Monopoly.java
+++ src/Monopoly.java
@@ -3,6 +3,10 @@ import javax.swing.SwingUtilities;
 public class Monopoly {
     public static void main(String[] args) {
         Board b = new Board();
+        Player a = new Player("Elliot", 1);
+        Player c = new Player("Brett", 2);
+        Display.players.add(a);
+        Display.players.add(c);
         //Graphics!
         SwingUtilities.invokeLater(() -> Display.setupFrame());
 
--- src/Player.java
+++ src/Player.java
@@ -2,25 +2,55 @@
 import java.util.ArrayList;
 
 public class Player {
-
+    private String name;
+    private int money;
+    private int location;
+    private ArrayList<Property> properties;
+    private boolean getOutJail;
+    private boolean jailed;
+    private int jailTurns;
+    private int turnOrder;
     //TODO FIX
+    public Player(String n, int turn){
+        name = n;
+        money = 1500;
+        location = 0;
+        properties = new ArrayList<Property>();
+        getOutJail = false;
+        jailed = false;
+        jailTurns = 0;
+        turnOrder = turn;
+        
+    }
+    public boolean getOJail(){
+        return getOutJail;
+    }
+    public int turnsServed(){
+        return jailTurns;
+    }
+    public boolean UinJail(){
+        return jailed;
+    }
     public String getName(){
-        return "no one";
+        return name;
+    }
+    public int Order(){
+        return turnOrder;
     }
 
     ///TODO FIX
     public int getMoney(){
-        return 1000;
+        return money;
     }
 
     //TODO FIX
     public ArrayList<Property> getProperties(){
-        return null;
+        return properties;
     }
 
     //TODO FIX
     //Go is the top left corner, location 0. Locations increase by 1 for each property that the player passes clockwise.
     public int getLocation(){
-        return 0;
+        return location;
     }
 }
